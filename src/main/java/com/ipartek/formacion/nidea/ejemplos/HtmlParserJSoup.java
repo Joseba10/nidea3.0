82f02e08ffa15a9d7619bbab84451b30d8bb274f
joseba <Curso@Formacion-TOS14.ipartekaula.com>
2018-04-13 14:27:31 +0200

    Crear un producto dinamicamente

--- src/main/java/com/ipartek/formacion/nidea/ejemplos/HtmlParserJSoup.java
+++ src/main/java/com/ipartek/formacion/nidea/ejemplos/HtmlParserJSoup.java
@@ -2,6 +2,12 @@ package com.ipartek.formacion.nidea.ejemplos;
 
 import java.io.IOException;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class HtmlParserJSoup {
 
 	public static void main(String[] args) throws IOException {
@@ -19,7 +25,22 @@ public class HtmlParserJSoup {
 		 * 
 		 * }
 		 */
-		String url = ("http://localhost:8080/nidea/login");
+
+		// String url = "http://192.168.0.15:8080/nidea/login";
+
+		String url = "http://localhost:8080/nidea/login";
+		String url2 = "http://localhost:8080/backoffice/materiales";
+		String url3 = "http://localhost:8080/backoffice/materiales?op=1";
+
+		Connection.Response response = Jsoup.connect(url).method(Connection.Method.POST).data("usuario", "admin")
+				.data("password", "admin").execute();
+
+		Document doc = response.parse();
+		System.out.println(doc.getElementsByTag("h1").get(0).text());
+
+		Element div = doc.getElementById("form-group");
+		Elements crear = doc.getElementsByTag("a");
+		Elements botton = doc.getElementsByTag("button");
 
 	}
 
