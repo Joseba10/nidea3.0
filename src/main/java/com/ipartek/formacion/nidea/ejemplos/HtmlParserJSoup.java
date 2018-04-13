2be4503b70b7926217b61a3d1a9d1fd5bb9a4156
joseba <Curso@Formacion-TOS14.ipartekaula.com>
2018-04-13 14:06:14 +0200

    JSoup

---
+++ src/main/java/com/ipartek/formacion/nidea/ejemplos/HtmlParserJSoup.java
@@ -0,0 +1,26 @@
+package com.ipartek.formacion.nidea.ejemplos;
+
+import java.io.IOException;
+
+public class HtmlParserJSoup {
+
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * Document doc = Jsoup.connect("http://example.com/").get(); String title =
+		 * doc.title(); Elements anclas = doc.getElementsByTag("a");
+		 * System.out.println("TITULO " + title);
+		 * 
+		 * for (Element ancla : anclas) {
+		 * 
+		 * String urlPagina2 = ancla.attr("href"); Document doc2 =
+		 * Jsoup.connect(urlPagina2).get(); System.out.println("titulo pagina 2 " +
+		 * doc2.title());
+		 * 
+		 * }
+		 */
+		String url = ("http://localhost:8080/nidea/login");
+
+	}
+
+}
